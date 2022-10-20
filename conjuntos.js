f12278ead3070377f66e6de66ba96f2f1c74825c
Marianne Garrido <64554585+Marianne-90@users.noreply.github.com>
2022-10-20 14:53:54 +0000

    Initial commit

---
+++ conjuntos.js
@@ -0,0 +1,13 @@
+familia = ["Rosi","Lars","Markus","Sami","Marianne"]
+
+const miSet = new Set(familia)
+
+console.log(miSet)
+
+miSet.add("Marianne")
+
+console.log(miSet)
+
+miSet.add("Javascript")
+
+console.log(miSet)
